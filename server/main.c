c20c9da3530da43b96da883e5ed536061db99a20
vonNeumann <vonNeumann@Hadewijch.com>
2025-02-26 17:45:55 +0600

    Integrate MySQL in the server
    Signed-off-by: vonNeumann <vonNeumann@Hadewijch.com>

--- server/main.c
+++ server/main.c
@@ -3,7 +3,62 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <mysql/mysql.h>
 
+#define SERVER_PORT 8080
+#define BUFFER_SIZE 1024
+
+void create_table(MYSQL *conn) {
+    const char *create_table_query = 
+        "CREATE TABLE IF NOT EXISTS users ("
+        "user_id INT PRIMARY KEY, "
+        "first_name VARCHAR(50) NOT NULL, "
+        "last_name VARCHAR(50) NOT NULL, "
+        "email VARCHAR(100) NOT NULL UNIQUE, "
+        "city VARCHAR(50) NOT NULL);";
+
+    if (mysql_query(conn, create_table_query)) {
+        fprintf(stderr, "MySQL query failed: %s\n", mysql_error(conn));
+    } else {
+        printf("Table 'users' created successfully.\n");
+    }
+}
+
+void store_user_data(char *data) {
+    MYSQL *conn = mysql_init(NULL);
+    if (!conn) {
+        fprintf(stderr, "MySQL initialization failed\n");
+        return;
+    }
+    
+    if (!mysql_real_connect(conn, "db-mysql", "myuser", "user_password", "mydatabase", 3306, NULL, 0)) {
+        fprintf(stderr, "MySQL connection failed: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        return;
+    }
+    static int called = 0;
+    if(!called) {
+        printf("setting up table\n");
+        create_table(conn);
+        called = 1;
+    }
+
+    char first_name[50], last_name[50], email[100], city[50];
+    int user_id;
+
+    sscanf(data, "%d,%49[^,],%49[^,],%99[^,],%49[^,]", &user_id, first_name, last_name, email, city);
+
+    char query[BUFFER_SIZE];
+    snprintf(query, sizeof(query), "INSERT INTO users (user_id, first_name, last_name, email, city) VALUES (%d, '%s', '%s', '%s', '%s')",
+             user_id, first_name, last_name, email, city);
+    
+
+    if (mysql_query(conn, query)) {
+        fprintf(stderr, "MySQL query failed: %s\n", mysql_error(conn));
+    }
+    
+    mysql_close(conn);
+}
 
 void start_server() {
     int server_sock, client_sock;
@@ -38,6 +93,7 @@ void start_server() {
     while ((client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size)) >= 0) {
         while (recv(client_sock, buffer, BUFFER_SIZE, 0) > 0) {
             printf("Received data: %s\n", buffer);
+            store_user_data(buffer);
             send(client_sock, "ACK", strlen("ACK"), 0);
             memset(buffer, 0, BUFFER_SIZE);
         }
